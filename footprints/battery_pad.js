fa45410680dcfe9b22debaa6cada1d80bfc16d23
tarneo <tarneo@tarneo.fr>
2023-10-24 14:43:30 +0200

    Add spacing between battery pads (avoid shorts)

--- footprints/battery_pad.js
+++ footprints/battery_pad.js
@@ -4,37 +4,34 @@
 
 module.exports = {
     params: {
-        designator: 'D',
+        designator: 'BATTPAD',
         plus: undefined,
-        minus: undefined
+        minus: undefined,
+        spacing: 10, // mm, between the two pads. Should be as much as possible to avoid short circuits
+        symbol_size: 1.5, // mm, from center to end of each line of plus symbol
+        pad_size: 1.5, // mm, size of each pad
     },
     body: p => `
   
     (module ComboDiode (layer F.Cu) (tedit 5B24D78E)
-
-
         ${p.at /* parametric position */}
 
         ${'' /* footprint reference */}
         (fp_text reference "${p.ref}" (at 0 0) (layer F.SilkS) ${p.ref_hide} (effects (font (size 1.27 1.27) (thickness 0.15))))
         (fp_text value "" (at 0 0) (layer F.SilkS) hide (effects (font (size 1.27 1.27) (thickness 0.15))))
         
-        ${''/* plus symbol */}
-        (fp_line (start -3 0) (end -4 0) (layer F.SilkS) (width .1))
-        (fp_line (start -3.5 -.5) (end -3.5 .5) (layer F.SilkS) (width .1))
-        (fp_line (start -3 0) (end -4 0) (layer B.SilkS) (width .1))
-        (fp_line (start -3.5 -.5) (end -3.5 .5) (layer B.SilkS) (width .1))
+        ${
+          // Bottom and top layers
+          ['F', 'B'].map(l => `
+            ${'' /* plus symbol */}
+            (fp_line (start ${-p.spacing/2-p.symbol_size} 0) (end ${-p.spacing/2+p.symbol_size} 0) (layer ${l}.SilkS) (width .1))
+            (fp_line (start ${-p.spacing/2} ${-p.symbol_size}) (end ${-p.spacing/2} ${p.symbol_size}) (layer ${l}.SilkS) (width .1))
 
-        ${''/* (fp_line (start -0.75 0) (end -0.35 0) (layer F.SilkS) (width 0.1))
-        (fp_line (start -0.75 0) (end -0.35 0) (layer B.SilkS) (width 0.1))*/}
-
-    
-        ${''/* SMD pads on both sides */}
-        (pad 1 smd rect (at -1.65 0 ${p.rot}) (size 1.5 1.5) (layers F.Cu F.Paste F.Mask) ${p.plus.str})
-        (pad 2 smd rect (at 1.65 0 ${p.rot}) (size 1.5 1.5) (layers B.Cu B.Paste B.Mask) ${p.minus.str})
-        (pad 1 smd rect (at -1.65 0 ${p.rot}) (size 1.5 1.5) (layers B.Cu B.Paste B.Mask) ${p.plus.str})
-        (pad 2 smd rect (at 1.65 0 ${p.rot}) (size 1.5 1.5) (layers F.Cu F.Paste F.Mask) ${p.minus.str})
+            ${'' /* pads */}
+            (pad 1 smd rect (at ${-p.spacing/2} 0 ${p.rot}) (size ${p.pad_size} ${p.pad_size}) (layers ${l}.Cu ${l}.Paste ${l}.Mask) ${p.plus.str})
+            (pad 2 smd rect (at ${p.spacing/2} 0 ${p.rot}) (size ${p.pad_size} ${p.pad_size}) (layers ${l}.Cu ${l}.Paste ${l}.Mask) ${p.minus.str})
+          `).join('')
+        }
     )
-  
-    `
+  `
 }
